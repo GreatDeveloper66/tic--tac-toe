eae8eb3d7a68b2926e68d45ee10d332fd9079cff
Adam <adam@mac.attlocal.net>
2019-10-25 18:45:35 -0500

    Add Board and Status Child Components

--- src/App.js
+++ src/App.js
@@ -5,22 +5,37 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Status />
+      <Board />
     </div>
   );
 }
+
+class Board extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return(
+      <div className="Board">
+        Board
+      </div>
+    );
+  }
+}
+
+class Status extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return(
+      <div className="Status">
+        Status
+      </div>
+    );
+  }
+}
+
 
 export default App;
