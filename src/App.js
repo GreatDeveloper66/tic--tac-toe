81e49b1b3fde05d149961db0d28bb5c14e0940bb
Adam <adam@mac.attlocal.net>
2019-10-26 19:38:28 -0500

    Add onClick virtual event handler to Board

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
@@ -8,13 +7,20 @@ class App extends React.Component {
     this.state = {
       squares: ["","X","O","","","","","",""]
     }
+    this.clickSquare = this.clickSquare.bind(this);
+  }
+  clickSquare(i){
+    window.alert("hello hello hello");
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
   }
 
   render(){
     return(
       <div className="App">
         <Status />
-        <Board layout={this.state.squares} />
+        <Board layout={this.state.squares} onClick={() => this.clickSquare(0)}/>
       </div>
     );
   }
@@ -24,18 +30,19 @@ class Board extends React.Component {
   constructor(props){
     super(props);
   }
+
   render(){
     return(
-      <div className="Board">
-        <Square status={this.props.layout[0]}/>
-        <Square status={this.props.layout[1]}/>
-        <Square status={this.props.layout[2]}/>
-        <Square status={this.props.layout[3]}/>
-        <Square status={this.props.layout[4]}/>
-        <Square status={this.props.layout[5]}/>
-        <Square status={this.props.layout[6]}/>
-        <Square status={this.props.layout[7]}/>
-        <Square status={this.props.layout[8]}/>
+      <div className="Board" onClick={this.props.onClick}>
+        <Square status={this.props.layout[0]} />
+        <Square status={this.props.layout[1]} />
+        <Square status={this.props.layout[2]} />
+        <Square status={this.props.layout[3]} />
+        <Square status={this.props.layout[4]} />
+        <Square status={this.props.layout[5]} />
+        <Square status={this.props.layout[6]} />
+        <Square status={this.props.layout[7]} />
+        <Square status={this.props.layout[8]} />
       </div>
     );
   }
